69c6c3534aaa26c0759d8f933a7f3c52b8132bdd
Jesse Martinez <kroronos@ufl.edu>
2020-02-02 02:50:35 -0500

    Card Template
    Implemented Card Template UI element with scriptableObject behavior

---
+++ Assets/CreateCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateCard : MonoBehaviour
+{
+    public Card card;
+
+    public Text nameText;
+    public Text descriptionText;
+    public Text costText;
+    public Image artImage;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = card.cardName;
+        descriptionText.text = card.description;
+        costText.text = card.cost.ToString();
+
+        artImage.sprite = card.art;
+        
+    }
+}
